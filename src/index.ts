3343b62da227723c3fa7cdee358138670ac385d7
Elchin Huseynli <elchin.kv@gmail.com>
2023-07-04 07:41:01 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
-import { greetUser } from '$utils/greet';
+import 'swiper/swiper-bundle.css';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import Swiper from 'swiper';
+import { EffectFade, Navigation } from 'swiper';
+
+gsap.registerPlugin(ScrollTrigger);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const heroSlider = new Swiper('[data-slider-hero]', {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 5000,
+  },
+  parallax: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  effect: 'fade',
+  grabCursor: true,
+  fadeEffect: {
+    crossFade: true,
+  },
+  navigation: {
+    nextEl: '[data-hero-slider-controls="next"]',
+    prevEl: '[data-hero-slider-controls="prev"]',
+  },
+  modules: [EffectFade, Navigation],
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ToursSlider = new Swiper('[data-slider-tour]', {
+  loop: false,
+  speed: 800,
+  grabCursor: true,
+  autoplay: {
+    delay: 5000,
+  },
+  parallax: true,
+  slidesPerView: 1.8,
+  spaceBetween: 30,
+  centeredSlides: true,
+  //   autoHeight: true,
 });
